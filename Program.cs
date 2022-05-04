b008b65edfe3f4182e759685501860bf4ec01556
Jacob Pimental <Jacob16682@gmail.com>
2022-05-04 14:56:37 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+
+namespace BlackGuardDeobfuscator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ModuleContext modCtx = ModuleDef.CreateModuleContext();
+            ModuleDefMD module = ModuleDefMD.Load(@"C:\Users\Jacob\Desktop\blackguard.exe", modCtx);
+            Console.WriteLine(module.Assembly.Modules);
+            int unkMethodCount = 0;
+            foreach (var type in module.Types)
+            {
+                foreach (var attr in type.Fields)
+                {
+                    if(attr.FieldSig.Type.TypeName == "Byte[]")
+                    {
+                        Console.WriteLine("Name: " + attr.Name);
+                        Console.WriteLine("Initial Value: " + BitConverter.ToString( attr.InitialValue));
+                    }
+                }
+            }
+        }
+    }
+}
